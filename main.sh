78710e931bcf077cbf2cb3a31b40d1c1c52a15d3
LucidWraithh <razor0916@gmail.com>
2024-04-22 11:22:08 -0500

    got the first iteration of TextNode working

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-echo "hello world"
+python src/main.py
---
+++ src/textnode.py
@@ -0,0 +1,14 @@
+class TextNode:
+
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(node1, node2):
+        if node1.text == node2.text and node1.text_type == node2.text_type and node1.url == node2.url:
+            return True
+        return False
+    
+    def __repr__(node):
+        return (f"TextNode ({node.text}, {node.text_type}, {node.url})")
